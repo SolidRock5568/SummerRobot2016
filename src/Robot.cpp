541f0805fa359e59a16b82b5c65234e3e00aa9eb
Alexxel <alexx.long@yahoo.com>
2016-05-02 22:14:52 -0500

    Added in ports
    Added in basic 6 motor Ports, 2 encoder Ports
    
    -Alex

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -2,6 +2,9 @@
 
 class Robot: public IterativeRobot
 {
+
+
+
 private:
 	LiveWindow *lw = LiveWindow::GetInstance();
 	SendableChooser *chooser;
@@ -9,27 +12,112 @@ private:
 	const std::string autoNameCustom = "My Auto";
 	std::string autoSelected;
 
+	// Start Define User Input
+	Joystick mainJoystick;
+	// End Define User Input
+
+	//	Start Define User Input Ports
+
+	// End Define User Inputs Ports
+	const int MainJoystickPort = 0;
+	// Start Define Motor Control
+	Victor leftWheel;
+	Victor rightWheel;
+	Victor leftShoot;
+	Victor rightShoot;
+	Victor armLift;
+	Victor kicker;
+	// End Define Motor Control
+
+	// Start Define Motor Ports
+	const int LeftDriveMotorPort = 0;
+	const int RightDriveMotorPort = 1;
+	const int LeftShootMotorPort = 2;
+	const int RightShootMotorPort = 3;
+	const int ArmLiftMotorPort = 4;
+	const int KickerMotorPort = 5;
+	// End Define Motor Ports
+
+	// Start Define Sensor Types
+	Encoder rightEncoder;
+	Encoder leftEncoder;
+	// End Define Sensor Types
+
+	// Start Define Sensor Ports
+	const int RightEncoderPort1 = 0;
+	const int RightEncoderPort2 = 1;
+	const int LeftEncoderPort1 = 2;
+	const int LeftEncoderPort2 = 3;
+	const std::string GyroPort =  ""; // Replace with Gyro Port
+	const int DownArmLimitSwitchPort = 5;
+	const int UpArmLimitSwitchPort = 6;
+	// End Define Sensor Ports
+public:
+	Robot() :
+
+		 mainJoystick(MainJoystickPort),
+
+
+		 leftWheel(LeftDriveMotorPort),
+		 rightWheel(RightDriveMotorPort),
+		 leftShoot(LeftShootMotorPort),
+		 rightShoot(RightShootMotorPort),
+		 armLift(ArmLiftMotorPort),
+		 kicker(KickerMotorPort),
+
+
+		 rightEncoder(RightEncoderPort1, RightEncoderPort2, false, Encoder::k4X),
+		 leftEncoder(LeftEncoderPort1, LeftEncoderPort2, false, Encoder::k4X)
+
+{
+		rightEncoder.SetSamplesToAverage(5);
+		rightEncoder.SetDistancePerPulse(1.0 / 360.0 * 2.0 * 3.1415 * 1.5);
+		rightEncoder.SetMinRate(1.0);
+
+
+
+		leftEncoder.SetSamplesToAverage(5);
+		leftEncoder.SetDistancePerPulse(1.0 / 360.0 * 2.0 * 3.1415 * 1.5);
+		leftEncoder.SetMinRate(1.0);
+
+				// Defines the number of samples to average when determining the rate.
+				// On a quadrature encoder, values range from 1-255; larger values
+				//   result in smoother but potentially less accurate rates than lower values.
+
+
+				// Defines how far the mechanism attached to the encoder moves per pulse.
+				// In this case, we assume that a 360 count encoder is directly attached
+				//   to a 3 inch diameter (1.5inch radius) wheel, and that we want to
+				//   measure distance in inches.
+
+
+				// Defines the lowest rate at which the encoder will not be considered
+				//   stopped, for the purposes of the GetStopped() method.
+				// Units are in distance / second, where distance refers to the units
+				//   of distance that you are using, in this case inches.
+
+
+
+}
 	void RobotInit()
 	{
-		chooser = new SendableChooser();
+
+
+
+
+
+		/*chooser = new SendableChooser();
 		chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
 		chooser->AddObject(autoNameCustom, (void*)&autoNameCustom);
-		SmartDashboard::PutData("Auto Modes", chooser);
+		SmartDashboard::PutData("Auto Modes", chooser);*/
+
 	}
 
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the GetString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the if-else structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
-	 */
+
 	void AutonomousInit()
 	{
-		autoSelected = *((std::string*)chooser->GetSelected());
+		/*autoSelected = *((std::string*)chooser->GetSelected());
 		//std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
 		std::cout << "Auto selected: " << autoSelected << std::endl;
 
@@ -37,7 +125,7 @@ private:
 			//Custom Auto goes here
 		} else {
 			//Default Auto goes here
-		}
+		}*/
 	}
 
 	void AutonomousPeriodic()

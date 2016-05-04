7917c8ed9e4bdf7077a2cfd0f77a7f44f0c2c756
Alexxel <alexx.long@yahoo.com>
2016-05-03 21:01:05 -0500

    CAMERA
    Added in an Axis Camera, and an Auto mode chooser

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -9,8 +9,8 @@ class Robot: public IterativeRobot
 private:
 	LiveWindow *lw = LiveWindow::GetInstance();
 	SendableChooser *chooser;
-	const std::string autoNameDefault = "Default";
-	const std::string autoNameCustom = "My Auto";
+	const std::string autoNameDefault = "Default Auto Mode";
+	const std::string autoNameCustom = "Test Auto Selector";
 	std::string autoSelected;
 
 	// Start Define User Input
@@ -40,6 +40,12 @@ private:
 	// End Define Motor Ports
 
 	// Start Define Sensor Types
+			// Start Camera
+			IMAQdxSession session;
+			Image *frame;
+			IMAQdxError imaqError;
+			std::unique_ptr<AxisCamera> camera;
+			// End Camera
 	Encoder rightEncoder;
 	Encoder leftEncoder;
 	DigitalInput UpSwitch;
@@ -85,6 +91,9 @@ public:
 {
 		DriveGyro = new AHRS(SPI::Port::kMXP);
 
+		frame = imaqCreateImage(IMAQ_IMAGE_RGB, 0);
+		camera.reset(new AxisCamera("axis-camera.local"));
+
 		rightEncoder.SetSamplesToAverage(5);
 		rightEncoder.SetDistancePerPulse(1.0 / 360.0 * 2.0 * 3.1415 * 1.5);
 		rightEncoder.SetMinRate(1.0);
@@ -121,10 +130,10 @@ public:
 
 
 
-		/*chooser = new SendableChooser();
+		chooser = new SendableChooser();
 		chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
 		chooser->AddObject(autoNameCustom, (void*)&autoNameCustom);
-		SmartDashboard::PutData("Auto Modes", chooser);*/
+		SmartDashboard::PutData("Auto Modes", chooser);
 
 	}
 
@@ -132,7 +141,7 @@ public:
 
 	void AutonomousInit()
 	{
-		/*autoSelected = *((std::string*)chooser->GetSelected());
+		autoSelected = *((std::string*)chooser->GetSelected());
 		//std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
 		std::cout << "Auto selected: " << autoSelected << std::endl;
 
@@ -140,15 +149,21 @@ public:
 			//Custom Auto goes here
 		} else {
 			//Default Auto goes here
-		}*/
+		}
 	}
 
 	void AutonomousPeriodic()
 	{
 		if(autoSelected == autoNameCustom){
-			//Custom Auto goes here
+			camera->GetImage(frame);
+			imaqDrawShapeOnImage(frame, frame, { 10, 10, 100, 100 }, DrawMode::IMAQ_DRAW_VALUE, ShapeMode::IMAQ_SHAPE_OVAL, 0.0f);
+			CameraServer::GetInstance()->SetImage(frame);
+			Wait(0.05);
 		} else {
-			//Default Auto goes here
+			camera->GetImage(frame);
+						//imaqDrawShapeOnImage(frame, frame, { 10, 10, 100, 100 }, DrawMode::IMAQ_DRAW_VALUE, ShapeMode::IMAQ_SHAPE_OVAL, 0.0f);
+			CameraServer::GetInstance()->SetImage(frame);
+			Wait(0.05);
 		}
 	}
 
